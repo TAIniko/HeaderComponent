91c454c1e779aa7aa6bd39e325482b918770e10f
TAiniko <taisuke.sum@gmail.com>
2020-06-08 11:06:00 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>How to put PHP in HTML - Simple Example</title>
+</head>
+
+<body>
+  <header>
+    <?php include(dirname(__FILE__) . '/assets/_inc/header.html'); ?>
+  </header>
+  main
+  <footer>
+    <?php include(dirname(__FILE__) . '/assets/_inc/footer.html'); ?>
+  </footer>
+</body>
+
+</html>
